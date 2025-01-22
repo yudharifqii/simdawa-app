92405e1786fc3f3d47b08cf1249947879aa4ae4d
“yudharifqii” <yudha.rifki.ananta@gmail.com>
2025-01-22 21:45:59 +0800

    database

---
+++ beasiswa-app.sql
@@ -0,0 +1,291 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 22 Jan 2025 pada 14.45
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `beasiswa-app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `beasiswa`
+--
+
+CREATE TABLE `beasiswa` (
+  `id` int(11) NOT NULL,
+  `nama_beasiswa` varchar(50) DEFAULT NULL,
+  `tanggal_mulai` date DEFAULT NULL,
+  `tanggal_selesai` date DEFAULT NULL,
+  `jenis_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `beasiswa`
+--
+
+INSERT INTO `beasiswa` (`id`, `nama_beasiswa`, `tanggal_mulai`, `tanggal_selesai`, `jenis_id`) VALUES
+(1, 'Uniska Peduli', '2025-01-15', '2025-01-22', 3),
+(2, 'Beasiswa Balangan', '2025-01-17', '2025-01-24', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jenis_beasiswa`
+--
+
+CREATE TABLE `jenis_beasiswa` (
+  `id` int(11) NOT NULL,
+  `nama_jenis` varchar(50) DEFAULT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `jenis_beasiswa`
+--
+
+INSERT INTO `jenis_beasiswa` (`id`, `nama_jenis`, `keterangan`) VALUES
+(2, 'Prestasi', 'Prestasi'),
+(3, 'Olahraga', 'Olahraga');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pendaftaran_pengguna`
+--
+
+CREATE TABLE `pendaftaran_pengguna` (
+  `id` int(11) NOT NULL,
+  `no_pendaftaran` varchar(20) DEFAULT NULL,
+  `nama_lengkap` varchar(75) DEFAULT NULL,
+  `no_handphone` varchar(15) DEFAULT NULL,
+  `bukti_daftar` varchar(255) DEFAULT NULL,
+  `prodi_id` int(11) DEFAULT NULL,
+  `keterangan` enum('Belum Diverifikasi','Sudah Diverifikasi') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengajuan_beasiswa`
+--
+
+CREATE TABLE `pengajuan_beasiswa` (
+  `id` varchar(64) NOT NULL,
+  `tanggal_pengajuan` date DEFAULT NULL,
+  `pengguna_id` int(11) DEFAULT NULL,
+  `beasiswa_id` int(11) DEFAULT NULL,
+  `keterangan` enum('Diajukan','Ditolak','Diusulkan','Ditetapkan') DEFAULT 'Diajukan'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `peran` enum('ADMIN','USER','OPERATOR') DEFAULT NULL,
+  `login_terakhir` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `pendaftaran_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `persyaratan`
+--
+
+CREATE TABLE `persyaratan` (
+  `id` int(11) NOT NULL,
+  `nama_persyaratan` varchar(100) DEFAULT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `persyaratan`
+--
+
+INSERT INTO `persyaratan` (`id`, `nama_persyaratan`, `keterangan`) VALUES
+(1, 'KTP', 'KTP'),
+(2, 'Kartu Keluarga', 'Kartu Keluarga');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `persyaratan_beasiswa`
+--
+
+CREATE TABLE `persyaratan_beasiswa` (
+  `id` int(11) NOT NULL,
+  `beasiswa_id` int(11) DEFAULT NULL,
+  `persyaratan_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `persyaratan_pengajuan`
+--
+
+CREATE TABLE `persyaratan_pengajuan` (
+  `id` int(11) NOT NULL,
+  `pengajuan_id` int(11) DEFAULT NULL,
+  `persyaratan_id` int(11) DEFAULT NULL,
+  `tanggal_upload` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `nama_file` varchar(255) DEFAULT NULL,
+  `keterangan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `prodi`
+--
+
+CREATE TABLE `prodi` (
+  `id` int(11) NOT NULL,
+  `nama_prodi` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `prodi`
+--
+
+INSERT INTO `prodi` (`id`, `nama_prodi`) VALUES
+(1, 'Teknik Informatika'),
+(2, 'Sistem Informasi'),
+(3, 'Ilmu Hukum');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `beasiswa`
+--
+ALTER TABLE `beasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `jenis_beasiswa`
+--
+ALTER TABLE `jenis_beasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pendaftaran_pengguna`
+--
+ALTER TABLE `pendaftaran_pengguna`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pengajuan_beasiswa`
+--
+ALTER TABLE `pengajuan_beasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `persyaratan`
+--
+ALTER TABLE `persyaratan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `persyaratan_beasiswa`
+--
+ALTER TABLE `persyaratan_beasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `persyaratan_pengajuan`
+--
+ALTER TABLE `persyaratan_pengajuan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `prodi`
+--
+ALTER TABLE `prodi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `beasiswa`
+--
+ALTER TABLE `beasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `jenis_beasiswa`
+--
+ALTER TABLE `jenis_beasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `pendaftaran_pengguna`
+--
+ALTER TABLE `pendaftaran_pengguna`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `persyaratan`
+--
+ALTER TABLE `persyaratan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `persyaratan_beasiswa`
+--
+ALTER TABLE `persyaratan_beasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `persyaratan_pengajuan`
+--
+ALTER TABLE `persyaratan_pengajuan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `prodi`
+--
+ALTER TABLE `prodi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
